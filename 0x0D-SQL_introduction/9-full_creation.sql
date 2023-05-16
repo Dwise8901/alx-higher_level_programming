9805c3492d4e8adc43d116b9a25bdb9825966ff1
Matthew Ojelere <damokellymt@gmail.com>
2023-05-16 09:39:47 -0700

    Full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- Creates and fills a table second_table with attributes id, name and score.
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "Ayanfeoluwa", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Natasha", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Damola", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "Pemisire", 8);
